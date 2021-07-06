99cd5d45916d8e5327aea3162da7664b208910f2
ABDUL HAKIM BIN ABDUL RASHID <85743082+mc210413691@users.noreply.github.com>
2021-07-06 08:27:54 +0800

    Add files via upload

---
+++ technicalEmployee.java
@@ -0,0 +1,21 @@
+//Subject: ITVM5013 SOFTWARE DESIGN AND DEVELOPMENT
+//Group 3 Member:
+//1.CHAN YEE HONG
+//2.KEVIN ZACHARY AL ANBIAH PAUL
+//3.ABDUL HAKIM BIN ABDUL RASHID
+//Course: MASTER OF INFORMATION TECHNOLOGY
+//Group Assignment 2: Company Structure
+
+public abstract class technicalEmployee extends employee {
+    int successfulCheckIns;
+
+    public technicalEmployee(String name){
+        super(name, 75000);
+    }
+
+    public String employeeStatus(){
+        String result;
+        result = super.employeeStatus();
+        result result;
+    }
+}
